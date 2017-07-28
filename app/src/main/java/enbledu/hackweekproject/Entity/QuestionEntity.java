e70e883ba96ace67c55f3c8d6cc6d7b49380a299
enble du <384509085@qq.com>
2017-07-28 16:28:39 +0800

    entity

--- app/src/main/java/enbledu/hackweekproject/Entity/QuestionEntity.java
+++ app/src/main/java/enbledu/hackweekproject/Entity/QuestionEntity.java
@@ -5,4 +5,27 @@ package enbledu.hackweekproject.Entity;
  */
 
 public class QuestionEntity {
+    public boolean hasAnser;
+    public String answer;
+
+    public QuestionEntity(boolean hasAnser, String answer) {
+        this.hasAnser = hasAnser;
+        this.answer = answer;
+    }
+
+    public boolean isHasAnser() {
+        return hasAnser;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setHasAnser(boolean hasAnser) {
+        this.hasAnser = hasAnser;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
 }
--- app/src/main/java/enbledu/hackweekproject/Entity/RobotEntity.java
+++ app/src/main/java/enbledu/hackweekproject/Entity/RobotEntity.java
@@ -5,4 +5,43 @@ package enbledu.hackweekproject.Entity;
  */
 
 public class RobotEntity {
+    //性格常量
+    public static int CHARACTER_TULING = 0;
+    public static int CHARACTER_BAIDU = 1;
+    public static int CHARACTER_MOLI = 2;
+
+
+    private int character;
+    private int stopSpeakingNum;
+    private boolean isSpeakAoto;
+
+    public RobotEntity(int character, int stopSpeakingNum, boolean isSpeakAoto) {
+        this.character = character;
+        this.stopSpeakingNum = stopSpeakingNum;
+        this.isSpeakAoto = isSpeakAoto;
+    }
+
+    public int getCharacter() {
+        return character;
+    }
+
+    public int getStopSpeakingNum() {
+        return stopSpeakingNum;
+    }
+
+    public boolean isSpeakAoto() {
+        return isSpeakAoto;
+    }
+
+    public void setCharacter(int character) {
+        this.character = character;
+    }
+
+    public void setStopSpeakingNum(int stopSpeakingNum) {
+        this.stopSpeakingNum = stopSpeakingNum;
+    }
+
+    public void setSpeakAoto(boolean speakAoto) {
+        isSpeakAoto = speakAoto;
+    }
 }
